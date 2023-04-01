6d7d2671b6f7204e4c66f655ef6b05e3c8b75cc0
Ramco <ramazane791@gmail.com>
2023-04-01 19:26:49 +0300

    Changes: added new colors

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,13 +1,24 @@
 const Colors = {
-  white: "#FFFFFF",
-  black: "#000000",
-  red: "#FFA500",
-  orange: "#FFA500",
   yellow: "#FFFF00",
+  "yellow-green": "#9ACD32",
   green: "#00FF00",
-  cyan: "#00FFFF",
+  "blue-green": "#0D98BA",
   blue: "#0000FF",
+  "blue-violet": "#8A2BE2",
+  violet: "#F800FF",
+  "red-violet": "#C71585",
+  red: "#FF0000",
+  "red-orange": "#FF5349",
+  orange: "#FFA500",
+  "yellow-orange": "#FFAE42",
+  white: "#FFFFFF",
+  "light-gray": "#D3D3D3",
+  gray: "#808080",
+  "dark-gray": "#A9A9A9",
+  black: "#000000",
+  cyan: "#00FFFF",
   magenta: "#FF00FF",
+  blurple: "#5865F2",
 };
 
 type ColorOptions = keyof typeof Colors | HexToString;
@@ -28,7 +39,7 @@ export class EmbedBuilder {
   url?: string;
   title?: string;
   description?: string;
-  media?: Media;
+  media?: string;
   color?: string;
 
   constructor() {
@@ -60,7 +71,7 @@ export class EmbedBuilder {
     return this;
   }
 
-  setColour(color:ColorOptions): this {
+  setColour(color: ColorOptions): this {
     this.color = Colors[color] || color;
     return this;
   }
@@ -76,4 +87,3 @@ export class EmbedBuilder {
     };
   }
 }
-
--- dist/index.js
+++ dist/index.js
@@ -1,61 +1,71 @@
 const Colors = {
-    white: "#FFFFFF",
-    black: "#000000",
-    red: "#FF0000",
-    orange: "#FFA500",
-    yellow: "#FFFF00",
-    green: "#00FF00",
-    cyan: "#00FFFF",
-    blue: "#0000FF",
-    magenta: "#FF00FF",
-  };
-  
-  class EmbedBuilder {
-    icon_url;
-    url;
-    title;
-    description;
-    media;
-    colour;
-  
-    constructor() {
-      return this;
-    }
-  
-    setIconUrl(icon_url) {
-      this.icon_url = icon_url;
-      return this;
-    }
-  
-    setUrl(url) {
-      this.url = url;
-      return this;
-    }
-  
-    setTitle(title) {
-      this.title = title;
-      return this;
-    }
-  
-    setDescription(description) {
-      this.description = description;
-      return this;
-    }
-  
-    setMedia(media) {
-      this.media = media;
-      return this;
-    }
-  
-    setColour(color) {
-      this.colour = Colors[color] || color;
-      return this;
-    }
-  
-    toJson() {
-      return this;
-    }
+  yellow: "#FFFF00",
+  "yellow-green": "#9ACD32",
+  green: "#00FF00",
+  "blue-green": "#0D98BA",
+  blue: "#0000FF",
+  "blue-violet": "#8A2BE2",
+  violet: "#F800FF",
+  "red-violet": "#C71585",
+  red: "#FF0000",
+  "red-orange": "#FF5349",
+  orange: "#FFA500",
+  "yellow-orange": "#FFAE42",
+  white: "#FFFFFF",
+  "light-gray": "#D3D3D3",
+  gray: "#808080",
+  "dark-gray": "#A9A9A9",
+  black: "#000000",
+  cyan: "#00FFFF",
+  magenta: "#FF00FF",
+  blurple: "#5865F2",
+};
+
+class EmbedBuilder {
+  icon_url;
+  url;
+  title;
+  description;
+  media;
+  colour;
+
+  constructor() {
+    return this;
   }
-  
-  module.exports = { EmbedBuilder };
-  
+
+  setIconUrl(icon_url) {
+    this.icon_url = icon_url;
+    return this;
+  }
+
+  setUrl(url) {
+    this.url = url;
+    return this;
+  }
+
+  setTitle(title) {
+    this.title = title;
+    return this;
+  }
+
+  setDescription(description) {
+    this.description = description;
+    return this;
+  }
+
+  setMedia(media) {
+    this.media = media;
+    return this;
+  }
+
+  setColour(color) {
+    this.colour = Colors[color] || color;
+    return this;
+  }
+
+  toJson() {
+    return this;
+  }
+}
+
+module.exports = { EmbedBuilder };
